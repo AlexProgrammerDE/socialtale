43338e0cffa2509e62c1157bda0d9c3905bd7434
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-09-23 16:52:51 +0200

    Remove junk code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,8 @@
 import {Head, Html, Main, NextScript} from "next/document";
 
 const themeInitializerScript = `
-       (function () {
-         const theme = window.localStorage.getItem("theme") || "dark";
-         document.getElementById('documentBody').setAttribute('data-theme', theme);
-       })();
+       const theme = window.localStorage.getItem("theme") || "dark";
+       document.getElementById('documentBody').setAttribute('data-theme', theme);
    `;
 
 export default function Document() {
