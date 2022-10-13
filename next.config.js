322ae3079df0c2f61e129e72aabd7dc8af5e7710
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-10-13 09:27:54 +0200

    Use SWC minify

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    swcMinify: true,
     i18n: {
         locales: ["en-US"],
         defaultLocale: "en-US",
